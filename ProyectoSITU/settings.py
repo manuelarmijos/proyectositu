681b06a8224964e6a083caf491c03fe9b70bcf38
manolo1994 <msarmijiso@gmail.com>
2022-10-22 18:24:27 -0500

    statics

--- ProyectoSITU/settings.py
+++ ProyectoSITU/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "templates/static",
-    #os.path.join(BASE_DIR, 'static'),
+    #BASE_DIR / "templates/static",
+    os.path.join(BASE_DIR, 'templates/static'),
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
