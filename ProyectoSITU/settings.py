2452c8f66800b05e031626090db2877ef7b03fac
manolo1994 <msarmijiso@gmail.com>
2022-10-22 16:49:49 -0500

    comit settings

--- ProyectoSITU/settings.py
+++ ProyectoSITU/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 print('RUTA de mi proy',BASE_DIR)
 
 
@@ -116,7 +117,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "templates/static",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
